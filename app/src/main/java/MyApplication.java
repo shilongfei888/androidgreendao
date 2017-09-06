c58c3b276d5376525dcd21426ab1cfca9e0d62db
史隆飞 <13263121443@163.com>
2017-09-06 16:17:08 +0800

    大是的撒

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,54 @@
+import android.app.Application;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.greendao.DaoMaster;
+import com.example.greendao.DaoSession;
+
+/**
+ * Created by 史隆飞 on 2017/9/1.
+ */
+
+public class MyApplication extends Application {
+
+
+    private DaoMaster.DevOpenHelper mHelper;
+    private SQLiteDatabase db;
+    private DaoMaster mDaoMaster;
+    private DaoSession mDaoSession;
+
+    public static MyApplication myApplication;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        myApplication=this;
+        setDatabase();
+    }
+
+    private void setDatabase() {
+
+        // 通过 DaoMaster 的内部类 DevOpenHelper，你可以得到一个便利的 SQLiteOpenHelper 对象。
+        // 可能你已经注意到了，你并不需要去编写「CREATE TABLE」这样的 SQL 语句，因为 greenDAO 已经帮你做了。
+        // 注意：默认的 DaoMaster.DevOpenHelper 会在数据库升级时，删除所有的表，意味着这将导致数据的丢失。
+        // 所以，在正式的项目中，你还应该做一层封装，来实现数据库的安全升级。
+        mHelper = new DaoMaster.DevOpenHelper(this, "notes-db", null);
+        db = mHelper.getWritableDatabase();
+        // 注意：该数据库连接属于 DaoMaster，所以多个 Session 指的是相同的数据库连接。
+        mDaoMaster = new DaoMaster(db);
+        mDaoSession = mDaoMaster.newSession();
+
+
+    }
+
+    public static MyApplication getInstance(){
+        return myApplication;
+    }
+
+    public DaoSession getDaoSession() {
+        return mDaoSession;
+    }
+    public SQLiteDatabase getDb() {
+        return db;
+    }
+
+}
---
+++ app/src/main/java/bean/User.java
@@ -0,0 +1,43 @@
+package bean;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Transient;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * Created by 史隆飞 on 2017/9/1.
+ */
+
+
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String name;
+    @Transient
+    private int tem;
+    @Generated(hash = 873297011)
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    @Generated(hash = 586692638)
+    public User() {
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
